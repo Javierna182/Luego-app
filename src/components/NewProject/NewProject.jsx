54f14665359464a4500e763fc67e861a8b5382b0
Javier Navarro <javierna182@gmail.com>
2023-08-17 19:34:21 -0500

    AWS set up

--- src/components/NewProject/NewProject.jsx
+++ src/components/NewProject/NewProject.jsx
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import Stack from '@mui/material/Stack';
+import { readAndCompressImage } from 'browser-image-resizer';
 
+import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
@@ -22,6 +23,46 @@ function NewProject() {
     const [coverImage, setCoverImage] = useState('');
     const [carouselImage, setCarouselImage] = useState('');
     const { id } = useParams();// used to get the project id to edit   
+
+    // Selected image file name
+    const [fileName, setFileName] = useState('');
+    // Selected file type
+    const [fileType, setFileType] = useState('');
+    // Selected image file
+    const [selectedFile, setSelectedFile] = useState();
+    // Selected image preview
+    const [imagePreview, setImagePreview] = useState();
+    // Used to display uploaded images on the page
+    const [imageList, setImageList] = useState([]);
+    
+    const onFileChange = async (event) => {
+      // Access the selected file
+      const fileToUpload = event.target.files[0];
+  
+      // Resize and compress the image. Remove this if using something other
+      // than an image upload.
+      const copyFile = new Blob([fileToUpload], { type: fileToUpload.type, name: fileToUpload.name });
+      const resizedFile = await readAndCompressImage(copyFile, {
+        quality: 1.0,    // 100% quality
+        maxHeight: 1000, // max height of the image
+      });
+
+    // Limit to specific file types.
+    const acceptedImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
+
+    // Check if the file is one of the allowed types.
+    if (acceptedImageTypes.includes(fileToUpload.type)) {
+      // Resizing the image removes the name, store it in a separate variable
+      setFileName(encodeURIComponent(fileToUpload.name));
+      setFileType(encodeURIComponent(fileToUpload.type));
+      // Save the resized file
+      setSelectedFile(resizedFile);
+      // Create a URL that can be used in an img tag for previewing the image
+      setImagePreview(URL.createObjectURL(resizedFile));
+    } else {
+      alert('Please select an image');
+    }
+  }
 
     useEffect(() => {
       if (id) {
@@ -81,6 +122,8 @@ function NewProject() {
         setCoverImage('');
         // setCarouselImage('');
         goToHome();
+        setFileName();
+        setFileType();
       }
     };
 
@@ -108,6 +151,38 @@ function NewProject() {
 
     return (
       <main>
+        <div>
+      <form onSubmit={addProject}>
+        <input
+          type="file"
+          accept="image/*"
+          onChange={onFileChange}
+        />
+        {
+          imagePreview && (
+            <>
+              <br />
+              <br />
+              <p>Preview</p>
+              <img style={{maxHeight: '100px'}} src={imagePreview} />
+            </>
+          )
+        }
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+
+      <h2>Images</h2>
+      {
+        imageList.map(image => (
+          <div key={image.id}>
+            <div>{image.name}</div>
+            <div>{image.type}</div>
+            <img style={{ maxHeight: '200px' }} src={`api/images/${image.name}`} /> 
+          </div>
+        ))
+      }
+    </div>
         <Box
             sx={{
               display: 'flex',
@@ -125,8 +200,8 @@ function NewProject() {
         <Box sx={{ flexGrow: 1,width: '100%', maxWidth: 860  }}>
           <Grid container spacing={2}>
             <Grid item xs={5} >
-            <TextField
-              id="standard-multiline-flexible"
+            <TextField 
+              id="filled-multiline-static"
               label="Project Name"
               multiline
               maxRows={4}
@@ -136,6 +211,7 @@ function NewProject() {
                 setTitle(event.target.value)
               }}
             />
+            <h1></h1>
             {/* <p>Name:<input value={title} type="text" placeholder="New Name" onChange={(event) => {
               // console.log(event)
               setTitle(event.target.value)
@@ -164,7 +240,8 @@ function NewProject() {
               </Stack>
             </Grid>
             <Box sx={{ flexGrow: 1,width: '100%', maxWidth: 350}}>
-              <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+              <h2></h2>
+              <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 2 }}>
               <Grid item xs={6}>
               <p>Image<input value={coverImage} type="text" placeholder="Image" onChange={(event) => setCoverImage(event.target.value)}/></p>
               </Grid>
